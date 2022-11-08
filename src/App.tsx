464df545560912a031bec424285c206ef3251491
maksymprokhnitskyi <maksymprokhnitskyi@gmail.com>
2022-11-08 19:31:01 +0400

    write components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import OwlImage from "./Images/owl-face-11.webp";
+import {
+  Wrapper,
+  Row,
+  Header,
+  Image,
+  Form,
+  Search,
+  Button,
+} from "./components/styled/index";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  }
+
+  const getJokes = async (event: React.FormEvent<HTMLFormElement>) => {
+
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Row>
+          <Header>Joker</Header>
+          <Image src={OwlImage} alt="Owl image" />
+        </Row>
+        <Form onSubmit={getJokes}>
+          <Search
+            type="text"
+            placeholder="Search.." 
+            value={search}
+            onChange={handleChange}
+          />
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Wrapper>
     </div>
   );
 }
---
+++ src/components/styled/index.ts
@@ -0,0 +1,9 @@
+import Wrapper from './Wrapper';
+import Row from './Row';
+import Header from './Header';
+import Image from './Image';
+import Form from './Form';
+import Search from './Search';
+import Button from './Button';
+
+export {Wrapper , Row , Header , Image , Form , Search , Button} ; 
