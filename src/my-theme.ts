8d3051add725bad2b0543147f1e4fabd93804ff0
maksymprokhnitskyi <maksymprokhnitskyi@gmail.com>
2022-11-09 17:37:23 +0400

    Project Completed!

--- src/my-theme.ts
+++ src/my-theme.ts
@@ -3,8 +3,8 @@ import { DefaultTheme } from 'styled-components';
 const myTheme: DefaultTheme = {
   borderRadius: '5px',
   colors: {
-    main: '3C12FA',
-    secondary: '8A28DE',
+    main: '#3C12FA',
+    secondary: '#8A28DE',
     white: '#FFFFFF',
     purple: '#53239C',
   },
